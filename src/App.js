4b14f66944fe22a8a565b02a784b0039a8267aa1
bookbento <mrsarunpatbook3@gmail.com>
2025-10-19 12:50:21 +0700

    Lab_12

--- src/App.js
+++ src/App.js
@@ -1,42 +1,48 @@
 import React, { useState, useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
 import axios from 'axios';
+
+import { Routes, Route } from 'react-router-dom';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchProducts } from './features/Product/actions';
 
 import Navbar from './features/Navbar';
 import Container from './features/Container';
 import Home from './features/Home';
-import GlobalStyle from './features/GlobalStyle';
-import AddForm from './features/Product/AddForm';
+import AddForm from './features/Product/AddForm'
 import UpdateForm from './features/Product/UpdateForm';
 
+import GlobalStyle from './features/GlobalStyle';
+
 function App() {
-  const [products, setProducts] = useState([]);
+  const products = useSelector((state) => state.products);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     async function getProducts() {
       const products = await axios.get(
-        'https://apimocha.com/react-redux-class/products'
+        'https://68eb580776b3362414cd6a63.mockapi.io/products'
       );
-      setProducts(products.data);
+      dispatch(fetchProducts(products.data));
     }
 
     getProducts();
   }, []);
+
+  function addProduct(product) {
+    dispatch(addProduct(product));
+  }
 
   return (
     <>
       <GlobalStyle />
       <Navbar />
       <Container>
-        {products.length > 0 ? (
-          <Routes>
-            <Route path="/create-product" element={<AddForm />} />
-            <Route path="/update-product/:id" element={<UpdateForm />} />
-            <Route path="/" element={<Home products={products} />} />
-          </Routes>
-        ) : (
-          <div>Loading products....</div>
-        )}
+        <Routes>
+          <Route path="/create-product" element={<AddForm addProduct={addProduct} />} />
+          <Route path="/" element={<Home products={products} />} />
+          <Route path="/update-product/:id" element={<UpdateForm />} />
+        </Routes>
       </Container>
     </>
   );
--- src/features/Product/AddForm.js
+++ src/features/Product/AddForm.js
@@ -1,45 +1,63 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Fragment } from "react";
-const { useState } = React;
-function AddForm({ addProduct }) {
-  const [name, setName] = useState("");
-  const [imageURL, setImageURL] = useState("");
-  const [type, setType] = useState("");
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
-function submit(e){
-    e.preventDefault();
-    addProduct({name, imageURL, type});
-}
-  
+import { addProduct } from './actions';
+
+function AddForm() {
+  const [name, setName] = useState('');
+  const [imageURL, setImageURL] = useState('');
+  const [type, setType] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  function onSubmit(event) {
+    event.preventDefault();
+    dispatch(addProduct({ name, type, imageURL }));
+    navigate('/');
+  }
 
   return (
     <>
-  <h1>Add Product</h1>
-  <form id="create-form" onSubmit={submit} >
-    <div className="input-group">
-      <label htmlFor="name" >Name</label>
-      <input name="name" type="text" id="name" value={name} onChange={(e) => setName(e.target.value)}/>
-    </div>
+      <h1>Add Product</h1>
+      <form id="create-form" onSubmit={onSubmit}>
+        <div className="input-group">
+          <label htmlFor="name">Name</label>
+          <input
+            name="name"
+            type="text"
+            id="name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+        </div>
 
-    <div className=" input-group">
-      <label htmlFor="imageURL">Image URL</label>
-      <input name="imageURL" type="text" id="imageURL" value={imageURL}  onChange={(e) => setImageURL(e.target.value)} />
-    </div>
+        <div className=" input-group">
+          <label htmlFor="imageURL">Image URL</label>
+          <input
+            name="imageURL"
+            type="text"
+            id="imageURL"
+            value={imageURL}
+            onChange={(event) => setImageURL(event.target.value)}
+          />
+        </div>
 
-    <div className=" input-group">
-      <label htmlFor="type">Type</label>
-      <input name="type" type="text" id="type" value={type} onChange={(e) => setType(e.target.value)} />
-    </div>
+        <div className=" input-group">
+          <label htmlFor="type">Type</label>
+          <input
+            name="type"
+            type="text"
+            id="type"
+            value={type}
+            onChange={(event) => setType(event.target.value)}
+          />
+        </div>
 
-    <button type="submit">Add product</button>
-  </form>
-</>
-)}
-
-
-AddForm.propTypes = {
-  addProduct: PropTypes.func.isRequired,
+        <button type="submit">Add product</button>
+      </form>
+    </>
+  );
 }
 
 export default AddForm;
